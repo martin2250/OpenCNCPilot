dd523ce347bec2c99761d77d12aef47792058f12
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-04-21 17:23:31 +0100

    Added probe X Y offset
    - added probe X Y offset

--- OpenCNCPilot/Communication/Machine.cs
+++ OpenCNCPilot/Communication/Machine.cs
@@ -1175,7 +1175,8 @@ namespace OpenCNCPilot.Communication
 			LastProbePosMachine = ProbePos;
 
 			ProbePos -= WorkOffset;
-
+			ProbePos.X += Properties.Settings.Default.ProbeOffsetX;
+			ProbePos.Y += Properties.Settings.Default.ProbeOffsetY;
 			LastProbePosWork = ProbePos;
 
 			bool ProbeSuccess = success.Value == "1";
